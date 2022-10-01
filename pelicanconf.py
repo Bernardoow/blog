c56225d7428d9a543a8ca3de9d278d83dc49f485
Bernardo Gomes <bgomesdeabreu@gmail.com>
2022-10-01 20:22:37 -0300

    update dependencies

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,16 +1,12 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*- #
-from __future__ import unicode_literals
+AUTHOR = "Bernardo Gomes"
+SITENAME = "Blog do Bernardoow"
+SITEURL = "/blog"
 
-AUTHOR = 'Bernardo Gomes'
-SITENAME = 'Blog do Bernardoow'
-SITEURL = '/blog'
+PATH = "content"
 
-PATH = 'content'
+TIMEZONE = "America/Sao_Paulo"
 
-TIMEZONE = 'America/Sao_Paulo'
-
-DEFAULT_LANG = 'pt'
+DEFAULT_LANG = "pt"
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -23,17 +19,19 @@ AUTHOR_FEED_RSS = None
 LINKS = ()
 
 # Social widget
-SOCIAL = (('Github', 'https://github.com/Bernardoow'),
-          ('Linkedin', 'https://www.linkedin.com/in/bernardo-gomes/'),)
+SOCIAL = (
+    ("Github", "https://github.com/Bernardoow"),
+    ("Linkedin", "https://www.linkedin.com/in/bernardo-gomes/"),
+)
 
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+# RELATIVE_URLS = True
 
 # DEFAULT_METADATA = {
 #     'status': 'draft',
 # }
 
-
-GITHUB_URL = 'https://github.com/Bernardoow'
+DEFAULT_PAGINATION = 10
+GITHUB_URL = "https://github.com/Bernardoow"
